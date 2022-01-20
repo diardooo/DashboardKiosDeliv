04b712fc929eb2cdc8a1b62b3e376c9a725e554e
Lazuardi <lazuardi490@gmail.com>
2022-01-20 10:12:35 +0700

    Logout Confirmation Done

--- app/src/main/java/com/example/dashboardkiosdeliv/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/dashboardkiosdeliv/ChangePasswordActivity.kt
@@ -2,9 +2,10 @@ package com.example.dashboardkiosdeliv
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 
 class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +14,32 @@ class ChangePasswordActivity : AppCompatActivity() {
 
 //        setUpToolbar("Ganti Password")
         back()
+
+        var passLama = findViewById<TextView>(R.id.text_password_lama)
+        var passBaru = findViewById<TextView>(R.id.text_password_baru)
+        var konfirmasiPass = findViewById<TextView>(R.id.text_konfirmasi_password)
+        var btnGantiPass = findViewById<Button>(R.id.btn_ganti_password)
+
+        btnGantiPass.setOnClickListener {
+            if (passLama.text.toString().isEmpty()){
+                passLama.error = "Mohon masukkan password lama terlebih dahulu"
+                passLama.requestFocus()
+                return@setOnClickListener
+            }
+            if (passBaru.text.toString().isEmpty()){
+                passBaru.error = "Mohon masukkan password baru terlebih dahulu"
+                passBaru.requestFocus()
+                return@setOnClickListener
+            }
+            if (konfirmasiPass.text.toString().isEmpty()){
+                konfirmasiPass.error = "Mohon masukkan konfirmasi password terlebih dahulu"
+                konfirmasiPass.requestFocus()
+                return@setOnClickListener
+            }
+            intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun back(){
--- app/src/main/java/com/example/dashboardkiosdeliv/MenuActivity.kt
+++ app/src/main/java/com/example/dashboardkiosdeliv/MenuActivity.kt
@@ -1,10 +1,12 @@
 package com.example.dashboardkiosdeliv
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Window
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -19,37 +21,34 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        logout = findViewById(R.id.btn_logout)
-        changePassword = findViewById(R.id.btn_ganti_password)
+        logout = findViewById(R.id.btn_float_logout)
+        changePassword = findViewById(R.id.btn_float_ganti_password)
 
         logout.setOnClickListener {
             customDialog()
         }
 
         changePassword.setOnClickListener {
-            intent = Intent(this, ChangePasswordActivity::class.java)
-            startActivity(intent)
+            var intent1 = Intent(this, ChangePasswordActivity::class.java)
+            startActivity(intent1)
         }
 
     }
         fun customDialog(){
 
-            val btn_sudah = (findViewById<Button>(R.id.btn_logout_sudah))
-            val btn_belum = (findViewById<Button>(R.id.btn_logout_belum))
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.popup_logout_layout,null)
+            val mBuilder = androidx.appcompat.app.AlertDialog.Builder(this)
+                .setView(mDialogView)
+            mDialogView.setBackgroundResource(R.drawable.popup_bg)
+            val mAlertDialog = mBuilder.show()
+            mDialogView.findViewById<Button>(R.id.btn_logout_sudah).setOnClickListener {
+                mAlertDialog.dismiss()
+                var intent2 = Intent(this, LoginActivity::class.java)
+                startActivity(intent2)
+            }
+            mDialogView.findViewById<Button>(R.id.btn_logout_belum).setOnClickListener {
+                mAlertDialog.dismiss()
+            }
 
-            val dialog = Dialog(this)
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            dialog.setContentView(R.layout.popup_logout_layout)
-
-            dialog.show()
-
-//            btn_belum.setOnClickListener {
-//                dialog.dismiss()
-//            }
-//            btn_sudah.setOnClickListener {
-//                val intent = Intent(this, LoginActivity::class.java)
-//                startActivity(intent)
-//            }
         }
     }
