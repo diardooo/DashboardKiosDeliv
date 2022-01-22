3c6737e9c2f3e1f71b1f076f8330eda543d895e6
Lazuardi <lazuardi490@gmail.com>
2022-01-22 10:08:45 +0700

    XML Dashboard Staff Masketing

---
+++ app/src/main/java/com/example/dashboardkiosdeliv/DashboardStaffMarketingActivity.kt
@@ -0,0 +1,109 @@
+package com.example.dashboardkiosdeliv
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+import com.github.mikephil.charting.utils.MPPointF
+
+class DashboardStaffMarketingActivity : AppCompatActivity() {
+
+    private lateinit var pieChart1 : PieChart
+    private lateinit var pieChart2 : PieChart
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard_staff_marketing)
+
+        var btnBack = findViewById<ImageView>(R.id.img_back_dashb_staff_market)
+        pieChart1 = findViewById(R.id.chart_terbanyak_staff)
+        pieChart2 = findViewById(R.id.chart_gagal_staff)
+
+        btnBack.setOnClickListener {
+            intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initChart(chart : PieChart){
+
+        chart.setUsePercentValues(true)
+        chart.description.isEnabled = false
+        chart.setExtraOffsets(5f, 10f, 5f, 5f)
+
+        chart.dragDecelerationFrictionCoef = 0.95f
+
+        chart.setCenterTextTypeface(Typeface.DEFAULT_BOLD)
+        chart.setCenterTextSize(15f)
+
+        chart.isDrawHoleEnabled = true
+        chart.setHoleColor(Color.WHITE)
+
+        chart.setTransparentCircleColor(Color.WHITE)
+        chart.setTransparentCircleAlpha(110)
+
+        chart.holeRadius = 58f
+        chart.transparentCircleRadius = 61f
+
+        chart.setDrawCenterText(true)
+
+        chart.rotationAngle = 0f
+        chart.isRotationEnabled = true
+        chart.isHighlightPerTapEnabled = true
+
+        chart.animateY(1400, Easing.EaseInOutQuad)
+
+        val l = chart.legend
+        l.isEnabled = false
+        chart.setEntryLabelColor(Color.BLACK)
+        chart.setEntryLabelTextSize(12f)
+
+    }
+
+    private fun setChartData(dataSet: PieDataSet, chart: PieChart) {
+
+        dataSet.setDrawIcons(false)
+
+        dataSet.sliceSpace = 3f
+        dataSet.iconsOffset = MPPointF(0f, 40f)
+        dataSet.selectionShift = 5f
+
+        // add a lot of colors
+
+        // add a lot of colors
+        val colors = arrayListOf<Int>()
+
+        colors.add(ColorTemplate.rgb("#61d4b3"))
+        colors.add(ColorTemplate.rgb("#fdd365"))
+        colors.add(ColorTemplate.rgb("#f0134d"))
+        colors.add(ColorTemplate.rgb("#fb8d62"))
+        colors.add(ColorTemplate.rgb("#fd2eb3"))
+        colors.add(ColorTemplate.rgb("#ff677d"))
+        colors.add(ColorTemplate.rgb("#ff9d9d"))
+        colors.add(ColorTemplate.rgb("#633a82"))
+        colors.random()
+
+        dataSet.colors = colors
+
+        val data = PieData(dataSet)
+        data.setValueFormatter(PercentFormatter(chart))
+        data.setValueTextSize(12f)
+        data.setValueTextColor(Color.BLACK)
+        chart.data = data
+
+        // undo all highlights
+        chart.highlightValues(null)
+
+        chart.invalidate()
+
+    }
+
+}
