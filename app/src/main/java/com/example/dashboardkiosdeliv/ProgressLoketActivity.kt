e795c0796bd4bb6ef304bc06f9ad610dfd0c633d
Lazuardi <lazuardi490@gmail.com>
2022-02-10 21:30:48 +0700

    Add Line Chart at ProgressLoketActivity.kt

--- app/src/main/java/com/example/dashboardkiosdeliv/ProgressLoketActivity.kt
+++ app/src/main/java/com/example/dashboardkiosdeliv/ProgressLoketActivity.kt
@@ -7,8 +7,14 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ProgressLoketActivity : AppCompatActivity() {
 
@@ -16,6 +22,7 @@ class ProgressLoketActivity : AppCompatActivity() {
     private lateinit var btnDatePickerFrom : CardView
     private lateinit var tvDatePickerTo : TextView
     private lateinit var btnDatePickerTo : CardView
+    private lateinit var lineChart : LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +35,7 @@ class ProgressLoketActivity : AppCompatActivity() {
         btnDatePickerFrom = findViewById(R.id.btn_date_form_loket)
         tvDatePickerTo = findViewById(R.id.tv_date_to_loket)
         btnDatePickerTo = findViewById(R.id.btn_date_to_loket)
+        lineChart = findViewById(R.id.chart_progres_loket)
 
         //DATE PICKER
         val datePickerFrom = Calendar.getInstance()
@@ -68,6 +76,52 @@ class ProgressLoketActivity : AppCompatActivity() {
                 datePickerTo.get(Calendar.MONTH),
                 datePickerTo.get(Calendar.DAY_OF_MONTH)).show()
         }
+
+        setLineChartData()
+    }
+    private fun setLineChartData(){
+        val xValues = ArrayList<String>()
+        xValues.add("April, 2021")
+        xValues.add("May, 2021")
+        xValues.add("Jun, 2021")
+        xValues.add("Jul, 2021")
+        xValues.add("Aug, 2021")
+        xValues.add("Sep, 2021")
+        xValues.add("Oct, 2021")
+        xValues.add("Nov, 2021")
+        xValues.add("Dec, 2021")
+        xValues.add("Jan, 2022")
+        xValues.add("Feb, 2022")
+
+        val lineEntry = ArrayList<Entry>()
+        lineEntry.add(Entry(107F, 0))
+        lineEntry.add(Entry(86F, 1))
+        lineEntry.add(Entry(80F, 2))
+        lineEntry.add(Entry(55F, 3))
+        lineEntry.add(Entry(40F, 4))
+        lineEntry.add(Entry(33F, 5))
+        lineEntry.add(Entry(34F, 6))
+        lineEntry.add(Entry(28F, 7))
+        lineEntry.add(Entry(24F, 8))
+        lineEntry.add(Entry(161F, 9))
+        lineEntry.add(Entry(71F, 10))
+
+        val lineDataSet = LineDataSet(lineEntry, "Line Chart")
+        lineDataSet.color = resources.getColor(R.color.grayMuda)
+        lineDataSet.circleRadius = 0F
+        lineDataSet.setDrawFilled(true)
+        lineDataSet.fillAlpha = 30
+        lineDataSet.setDrawCubic(true)
+        lineDataSet.valueTextSize = 10f
+        lineDataSet.valueTextColor = R.color.black
+
+        val data = LineData(xValues, lineDataSet)
+
+        lineChart.data = data
+        lineChart.setBackgroundColor(resources.getColor(R.color.white))
+        lineChart.animateXY(3000,300)
+        lineChart.setDescription("")
+
     }
 
     private fun dateFrom(myCalendar: Calendar){
