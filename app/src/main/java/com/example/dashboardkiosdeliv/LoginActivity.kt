1fde0027576e3016f7ba49ab2aca1fc1e5659ae3
Lazuardi <lazuardi490@gmail.com>
2022-02-23 18:31:07 +0700

    update login logic & handler

--- app/src/main/java/com/example/dashboardkiosdeliv/LoginActivity.kt
+++ app/src/main/java/com/example/dashboardkiosdeliv/LoginActivity.kt
@@ -1,40 +1,116 @@
 package com.example.dashboardkiosdeliv
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dashboardkiosdeliv.databinding.ActivityLoginBinding
+import com.example.dashboardkiosdeliv.model.ResponseLogin
+import com.example.dashboardkiosdeliv.network.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
+    private var binding : ActivityLoginBinding? = null
+    private var email : String = ""
+    private var password : String = ""
+    private lateinit var profile : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
 
-        val username = findViewById<EditText>(R.id.et_username_login)
-        val password = findViewById<EditText>(R.id.et_password_login)
-        val login = findViewById<Button>(R.id.btn_login)
+        //Cek session
+        profile = getSharedPreferences("login_session", MODE_PRIVATE)
 
-        login.setOnClickListener {
-            if (username.text.toString().isEmpty()){
-                username.error = "Mohon masukkan email terlebih dahulu"
-                username.requestFocus()
-                return@setOnClickListener
+        if (profile.getString("email", null) != null){
+            if (profile.getString("level", null) == "1"){
+                startActivity(Intent(this,MenuDirekturActivity::class.java))
+                finish()
+
             }
-            if (password.text.toString().isEmpty()){
-                password.error = "Mohon masukkan password terlebih dahulu"
-                password.requestFocus()
-                return@setOnClickListener
+            if (profile.getString("level", null) == "2"){
+                startActivity(Intent(this, MenuStaffMarketingActivity::class.java))
+                finish()
+
             }
-            val intent = Intent(this,MenuDirekturActivity::class.java)
-            startActivity(intent)
+        }
+
+        binding!!.btnLogin.setOnClickListener {
+            email = binding!!.etUsernameLogin.text.toString()
+            password = binding!!.etPasswordLogin.text.toString()
+
+            when {
+                email == "" -> {
+                    binding!!.etUsernameLogin.error = "Email tidak boleh kosong"
+                }
+                password == "" -> {
+                    binding!!.etPasswordLogin.error = "Password tidak boleh kosong"
+                }
+                else -> {
+                    getData()
+                }
+            }
         }
     }
+
+    private fun getData(){
+
+        val api = RetrofitClient().getInstance()
+        api.login(email, password).enqueue(object : Callback<ResponseLogin>{
+
+            override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
+                if (response.isSuccessful) {
+                    if (response.body()?.response == true) {
+
+                        //Membuat session
+                        getSharedPreferences("login_session", MODE_PRIVATE)
+                            .edit()
+                            .putString("email", response.body()?.payload?.email)
+                            .putString("nama", response.body()?.payload?.nama)
+                            .putString("level", response.body()?.payload?.level_user)
+                            .putString("foto", response.body()?.payload?.foto)
+                            .apply()
+
+                        if (response.body()?.payload?.level_user == "1"){
+                            startActivity(Intent(this@LoginActivity, MenuDirekturActivity::class.java))
+                            finish()
+                        }
+                        if (response.body()?.payload?.level_user == "2"){
+                            startActivity(Intent(this@LoginActivity, MenuStaffMarketingActivity::class.java))
+                            finish()
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Login gagal, periksa kembali email dan password Anda",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                } else {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login gagal, terjadi kesalahan",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            override fun onFailure(call: retrofit2.Call<ResponseLogin>, t: Throwable) {
+                Log.e("pesan error","${t.message}")
+            }
+
+        })
+    }
+
     override fun onBackPressed() {
         moveTaskToBack(true)
     }
-
 
 }
