8ea85cebe87816d9c4aa72160a9831d90b1d5c86
Lazuardi <lazuardi490@gmail.com>
2022-02-23 18:25:56 +0700

    update profile activity

--- app/src/main/java/com/example/dashboardkiosdeliv/ProfileActivity.kt
+++ app/src/main/java/com/example/dashboardkiosdeliv/ProfileActivity.kt
@@ -1,20 +1,44 @@
 package com.example.dashboardkiosdeliv
 
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.example.dashboardkiosdeliv.databinding.ActivityProfileBinding
+import com.squareup.picasso.Picasso
 
 class ProfileActivity : AppCompatActivity() {
+
+    private var binding : ActivityProfileBinding? = null
+    private lateinit var profile : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
         var btnBack = findViewById<ImageView>(R.id.img_back_profile)
+        profile = getSharedPreferences("login_session", MODE_PRIVATE)
 
         btnBack.setOnClickListener {
             intent = Intent(this, MenuDirekturActivity::class.java)
             startActivity(intent)
         }
+
+        //Menampilkan data profile
+        binding?.tvNamaUser?.text = profile.getString("nama", null)
+        binding?.tvEmail?.text = profile.getString("email", null)
+        binding?.tvLevelUser?.text = if (profile.getString("level", null) == "1") "DIREKTUR" else "STAFF MARKETING"
+
+        if (binding?.tvLevelUser?.text == "DIREKTUR"){
+            binding?.tvLevelUser?.setTextColor(Color.parseColor("#F44336"))
+        } else{
+            binding?.tvLevelUser?.setTextColor(Color.parseColor("#2196F3"))
+        }
+
+        //Menampilkan foto profile dengan picasso
+        Picasso.get().load(profile.getString("foto", null)).into(binding?.imgProfile)
     }
 }
