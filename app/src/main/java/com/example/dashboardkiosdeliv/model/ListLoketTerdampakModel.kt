24a2ddb1b2d3dbc00471368e7b4af7f30495e1c7
Lazuardi <lazuardi490@gmail.com>
2022-03-03 06:49:50 +0700

    Add model recyclerview list loket terdampak

---
+++ app/src/main/java/com/example/dashboardkiosdeliv/model/ListLoketTerdampakModel.kt
@@ -0,0 +1,10 @@
+package com.example.dashboardkiosdeliv.model
+
+data class ListLoketTerdampakModel (
+    var foto : Int,
+    var loket : String,
+    var nomor : String,
+    var service : String,
+    var tanggal : String
+) {
+}
